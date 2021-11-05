663d0c694acb39a62dda583eb336c600dc90a74e
S.K.Obidul kadir <39410598+ObidulKadir@users.noreply.github.com>
2021-11-05 18:17:49 +0600

    updateed

--- interviewQ/array-split-splice.js
+++ interviewQ/array-split-splice.js
@@ -2,6 +2,9 @@ const array =[1,23,4,5,6,];
 console.log(array.slice(2,4))
 console.log(array.splice(2,4))
 // console.log(array.join("ami"))
+// console.log(array.split())//The split() method splits a string into an array of substrings, and returns the array.
+const str = "hello i am good boy!"
+console.log(str.split("i"));
 const newA = array.join("ami")
 
 console.log(newA)
--- interviewQ/undefine-null.js
+++ interviewQ/undefine-null.js
@@ -1,4 +1,4 @@
-//undefine
+//undefined
 // let num;
 // function add(num1,num2){
 //     console.log(num1,num2)
